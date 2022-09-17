daa2fce278fd3e9350b5f986186f89e6e74bbbe4
Stas V <stasvoi2004@gmail.com>
2022-09-17 19:04:01 +0300

    task 6

---
+++ task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int rem = a % 2;
+if(rem == 0)
+{
+    Console.Write($"Число {a} четное");
+}
+else
+{
+    Console.Write($"Число {a} нечетное");
+}
