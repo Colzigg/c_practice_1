0b3e99c0136cd113c3badba8853e245d9809a3f5
Stas V <stasvoi2004@gmail.com>
2022-09-17 19:04:23 +0300

    task 8

---
+++ task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+//int rem = a % 2;
+int count = 1;
+string result = "";
+int rem = 0;
+while(count <= a)
+{
+    rem = count % 2;
+    if(rem == 0)
+    {
+        result += $"{count}, ";
+    }
+    count++;
+}
+result = result.TrimEnd(' ', ',');
+Console.Write($"Последовательность чисел: {result}");
