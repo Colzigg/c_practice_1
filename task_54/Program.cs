3c43f89e727486019f1dec7c08937bb52a256aee
Stas V <stasvoi2004@gmail.com>
2022-10-12 11:24:23 +0300

    task 54 added

---
+++ task_54/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+double[,] MatrixInputDouble(int row, int column)
+{
+    double[,] matrix = new double[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 9);
+        }
+    }
+    return matrix;
+}
+
+void MatrixOutput(double[,] matrix)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Сгенерирована следующая матрица, размером {matrix.GetLength(0)} х {matrix.GetLength(1)}");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] >= 0) Console.Write(" " + matrix[i, j] + " ");
+            else Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MatrixRowMax(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, j])
+                {
+                    double tmp = matrix[i, k];
+                    matrix[i, k] = matrix[i, j];
+                    matrix[i, j] = tmp;
+                }
+            }
+
+        }
+    }
+    return matrix;
+}
+
+int rowMatrix = new Random().Next(4, 7);
+int columnMatrix = new Random().Next(3, 7);
+
+double[,] matrix = MatrixInputDouble(rowMatrix, columnMatrix);
+MatrixOutput(matrix);
+//double[,] matrixModified = MatrixRowMax(matrix);
+MatrixOutput(MatrixRowMax(matrix));
